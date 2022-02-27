b47f21885983d7078db69b62db436ad8d80b17fa
oscarcodefabric <oscara@codefabric.io>
2022-02-27 00:57:23 -0400

    Add SearchBar component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Input } from 'antd'
+import Icon from '@ant-design/icons'
+
+import { ReactComponent as SearchIcon } from 'assets/search.svg'
+import './styles.css'
+
+const { Search } = Input
+
+const SearchBar = ({ setSearchTerm }) => {
+  const handleSearch = (e) => {
+    setSearchTerm(e.toLowerCase())
+  }
+
+  return (
+    <div className="search-bar-container">
+      <Search
+        addonBefore={<Icon className="search-icon" component={SearchIcon} />}
+        className="search-bar"
+        placeholder="Artists Names &amp; Styles"
+        enterButton={<button className="search-button">Search</button>}
+        onSearch={handleSearch}
+      />
+    </div>
+  )
+}
+
+SearchBar.propTypes = {
+  setSearchTerm: PropTypes.func,
+}
+
+SearchBar.defaultProps = {
+  setSearchTerm: () => {},
+}
+
+export default SearchBar
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header/Header'
+export { default as SearchBar } from './SearchBar/SearchBar'
 export { default as UserCard } from './UserCard/UserCard'
 export { default as UsersGrid } from './UsersGrid/UsersGrid'
